d95c95e8ec8606d80d9bfa0b7012c92cc85688a2
lydiaralexander <89818941+lydiaralexander@users.noreply.github.com>
2022-01-09 20:43:08 -0500

    updating for deliverable 3 data and images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,16 @@ Suspension_Coil <- read.csv('Suspension_Coil.csv') #import dataset
 total_summary <- Suspension_Coil %>% summarize('Mean'=mean(PSI), 'Median'=median(PSI), 'Variance'=var(PSI), 'SD'=sd(PSI))
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize('Mean'=mean(PSI), 'Median'=median(PSI), 'Variance'=var(PSI), 'SD'=sd(PSI))
+
+#Deliverable 3
+
+t.test(Suspension_Coil$PSI, mu=1500)
+
+#lot 1
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+#lot 2
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+#lot 3
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
+
